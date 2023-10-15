1aea04ca853bd67634b9a57ad1a31b1b27cc8e1f
Tamar Hizler <tamarl8846@gmail.com>
2023-10-15 23:45:30 +0300

    Adding the --note to the options. the real bahave is not working. I only add check for the bollean value

--- Files Bundler/Program.cs
+++ Files Bundler/Program.cs
@@ -1,11 +1,11 @@
-﻿/*//exe1 bundle --output D:\folder\bundleFile.txt
+﻿//exe1 bundle --output D:\folder\bundleFile.txt
 using System.CommandLine;
 
 
 var rootCommand = new RootCommand("Root command for file bundler CLI");
 var bundleCommand = new Command("bundle", "Bundle code files to a single file");
 var bundleOutputOption = new Option<FileInfo>("--output", "file path and name");
-var bundleNoteOption = new Option<bool>("--note", "file path and name");
+var bundleNoteOption = new Option<bool>("--note", "Include source code references as comments in the bundled fil");
 var bundleLangOption = new Option<List<string>>("--lang", "required langueges for the bundled output")
 {
     IsRequired = true,
@@ -14,102 +14,33 @@ var bundleLangOption = new Option<List<string>>("--lang", "required langueges fo
 bundleCommand.AddOption(bundleOutputOption);
 bundleCommand.AddOption(bundleLangOption);
 
-bundleCommand.SetHandler((output) =>
+bundleCommand.SetHandler((output, langueges, note ) =>
 {
     try
     {
-        Console.WriteLine(output.Name);
+        Console.WriteLine("hello " + output.Name);
 
         File.Create(output.FullName);
         Console.WriteLine("file created successfully" + " " + string.Join(", ", bundleLangOption));
-
+        using (StreamWriter writer = new StreamWriter(output.FullName))
+        {
+            writer.WriteLine("Hello new file!");
+        }
+        if (langueges.Any()) //check the lang
+        {
+            Console.WriteLine("there is langs in!");
+        }
+        if (note == true) //check the note
+        {
+            Console.WriteLine("note option is true");
+        }
     }
     catch (DirectoryNotFoundException ex)
     {
         Console.WriteLine("ERROR: file path is invalid");
     }
-}, bundleOutputOption);
-
-bundleCommand.Handler = CommandHandler.Create<FileInfo, List<string>>((output, languages) =>
-{
-    try
-    {
-        File.Create(output.FullName);
-        Console.WriteLine("File created successfully");
-
-        if (languages.Any())
-        {
-            Console.WriteLine("List of selected languages:");
-            foreach (var language in languages)
-            {
-                Console.WriteLine(language);
-            }
-        }
-        else
-        {
-            Console.WriteLine("No languages selected.");
-        }
-    }
-    catch (DirectoryNotFoundException ex)
-    {
-        Console.WriteLine("ERROR: File path is invalid");
-    }
-});
-rootCommand.AddCommand(bundleCommand);
-rootCommand.InvokeAsync(args);*/
-
-using System;
-using System.CommandLine;
-using System.CommandLine.Invocation; // Import the Invocation namespace
-using System.IO;
-using System.Linq;
-using System.Collections.Generic; // Import the namespace for List
-
-var rootCommand = new RootCommand("Root command for file bundler CLI");
-var bundleCommand = new Command("bundle", "Bundle code files to a single file");
-
-var bundleOutputOption = new Option<FileInfo>("--output", "file path and name");
-var bundleNoteOption = new Option<bool>("--note", "Include source code references as comments in the bundled file"); // Added note option
-var bundleLangOption = new Option<List<string>>("--lang", "required languages for the bundled output")
-{
-    IsRequired = true,
-};
-
-bundleCommand.AddOption(bundleOutputOption);
-bundleCommand.AddOption(bundleLangOption);
-bundleCommand.AddOption(bundleNoteOption);
-
-bundleCommand.Handler = CommandHandler.Create<FileInfo, List<string>, bool>((output, languages, includeNotes) =>
-{
-    try
-    {
-        File.Create(output.FullName);
-        Console.WriteLine("File created successfully");
-
-        if (includeNotes)
-        {
-            Console.WriteLine("Including source code references as comments.");
-        }
-
-        if (languages.Any())
-        {
-            Console.WriteLine("List of selected languages:");
-            foreach (var language in languages)
-            {
-                Console.WriteLine(language);
-            }
-        }
-        else
-        {
-            Console.WriteLine("No languages selected.");
-        }
-    }
-    catch (DirectoryNotFoundException ex)
-    {
-        Console.WriteLine("ERROR: File path is invalid");
-    }
-});
+}, bundleOutputOption, bundleLangOption, bundleNoteOption);
 
 
 rootCommand.AddCommand(bundleCommand);
-rootCommand.Invoke(args);
+rootCommand.InvokeAsync(args);
