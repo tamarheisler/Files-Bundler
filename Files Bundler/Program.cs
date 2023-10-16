c4f98a753700553effc36fb505f3c6e8694c68c2
Tamar Hizler <tamarl8846@gmail.com>
2023-10-16 23:59:47 +0300

    all options work. Just have to implement the real action.

--- Files Bundler/Program.cs
+++ Files Bundler/Program.cs
@@ -1,5 +1,4 @@
-﻿//exe1 bundle --output D:\folder\bundleFile.txt
-using Files_Bundler;
+﻿using Files_Bundler;
 using System.CommandLine;
 
 
@@ -7,51 +6,88 @@ var rootCommand = new RootCommand("Root command for file bundler CLI");
 var bundleCommand = new Command("bundle", "Bundle code files to a single file");
 var bundleOutputOption = new Option<FileInfo>("--output", "file path and name");
 var bundleNoteOption = new Option<bool>("--note", "Include source code references as comments in the bundled fil");
-var bundleLangOption = new Option<List<string>>("--lang", "required langueges for the bundled output")
+var bundleLangOption = new Option<string>("--lang", "required langueges for the bundled output")
 {
     IsRequired = true,
 };
-//var bundleSortOption = new Option<>("--sort", "file path and name");
-//var bundleRemoveOption = new Option<>("--remove-empty-lines", "Remove empty lines in the code files");
+var bundleSortOption = new Option<bool>("--sort", "file path and name"); //if true- sort by program lang, else- by name of file 
+var bundleRemoveOption = new Option<bool>("--remove-empty-lines", "Remove empty lines in the code files");
 var bundleAuthorOption = new Option<string>("--author", "file author name");
 
 
 bundleCommand.AddOption(bundleOutputOption);
 bundleCommand.AddOption(bundleLangOption);
+bundleCommand.AddOption(bundleNoteOption);
+bundleCommand.AddOption(bundleAuthorOption);
+bundleCommand.AddOption(bundleRemoveOption);
+bundleCommand.AddOption(bundleSortOption);
 
-bundleCommand.SetHandler((output, langueges, note , author) =>
+bundleCommand.SetHandler((output, langueges, note, author, sort, remove) =>
 {
     try
     {
         Console.WriteLine("hello " + output.Name);
-        List<string>  filesNames = Functions.DirSearch(Directory.GetCurrentDirectory()); // list of all the code files names in current folder
-
+        List<string> filesNames = Functions.DirSearch(Directory.GetCurrentDirectory()); // list of all the code files names in current folder
         File.Create(output.FullName).Close();
-        Console.WriteLine("file created successfully" + " " + string.Join(", ", bundleLangOption));
+
+        /*foreach (var file in filesNames) // checks if the function of finding all the code files is working or not
+        {                                  // and also the func of the extensions.  both works!
+            Console.WriteLine(file + ", ");
+            Console.WriteLine(Functions.GetLanguageFromExtension(file) + ", ");
+        }*/
+
         using (StreamWriter writer = new StreamWriter(output.FullName))
         {
             writer.WriteLine("Hello new file!");
         }
-        if (langueges.Any()) //check the lang
+
+        // -----------------Langs option:
+        if (langueges == "all") 
         {
-            Console.WriteLine("there is langs in!");
+            Console.WriteLine("all langs");
+        }
+        else //the user want a list of specific langs
+        {
+            List<string> givenLangs = langueges.Split(' ').ToList();
+            List<string> validProgrammingLanguages = Functions.GetListOfProgrammingLanguages();
+            bool givenLangsValidation = Functions.checkAllGivenLangsValidation(givenLangs, validProgrammingLanguages);
+            if (!givenLangsValidation)
+            {
+                throw new Exception("ERROR: one or more of the given langueges is invalid");
+            }
+            Console.WriteLine("there is langs in!, you don't want all");
+        }
+
+        // -----------------Note option:
+        if (note == true) //check the note
+        {
+
+            Console.WriteLine("note option is true");
+        }
+        // -----------------Sort option:
+        if (sort == true)
+        {
+            Console.WriteLine("sort option is true");
+            // sort by the programming languages
         }
         else
         {
-            // add all code files of all programming langueges
+            // sort by A -Z
         }
-        if (note == true) //check the note
+        if (remove == true)
         {
-            Console.WriteLine("note option is true");
+            // remove lines in the final file
         }
-
     }
     catch (DirectoryNotFoundException ex)
     {
         Console.WriteLine("ERROR: file path is invalid");
     }
-}, bundleOutputOption, bundleLangOption, bundleNoteOption, bundleAuthorOption);
-
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}, bundleOutputOption, bundleLangOption, bundleNoteOption, bundleAuthorOption, bundleSortOption, bundleRemoveOption);
 
 rootCommand.AddCommand(bundleCommand);
 rootCommand.InvokeAsync(args);
--- Files Bundler/functions.cs
+++ Files Bundler/functions.cs
@@ -8,7 +8,7 @@ namespace Files_Bundler
 {
     public static class Functions
     {
-
+        //function that returns all the files that are in the binary tree of the given directory name.
         public static List<string> DirSearch(string sDir)
         {
             List<string> files = new List<String>();
@@ -30,5 +30,95 @@ namespace Files_Bundler
 
             return files;
         }
+
+
+
+        // function that gets 
+
+        public static List<string> GetListOfProgrammingLanguages()
+        {
+            // Create and return a list of programming languages
+            return new List<string> { "C", "C++", "C#", "Java", "Python", "JavaScript", "Ruby", "Swift", "Kotlin", "Rust", "Go", "PHP", "Perl", "TypeScript", "Dart", "R", "MATLAB", "Fortran", "COBOL", "Ada", "Lisp", "Scheme", "Prolog", "Haskell", "Erlang", "Groovy", "Scala", "Ruby on Rails", "HTML/CSS", "SQL", "Assembly language", "VHDL", "Verilog", "Scratch", "Blockly" };
+
+        }
+
+        public static string GetLanguageFromExtension(string fileName)
+        {
+            string extension = Path.GetExtension(fileName).TrimStart('.').ToLower();
+
+            // Define a dictionary to map file extensions to programming languages
+            Dictionary<string, string> extensionToLanguage = new Dictionary<string, string>
+        {
+            { "c", "C" },
+            { "cpp", "C++" },
+            { "cs", "C#" },
+            { "java", "Java" },
+            { "py", "Python" },
+            { "js", "JavaScript" },
+            { "rb", "Ruby" },
+            { "swift", "Swift" },
+            { "kt", "Kotlin" },
+            { "rs", "Rust" },
+            { "go", "Go" },
+            { "php", "PHP" },
+            { "pl", "Perl" },
+            { "ts", "TypeScript" },
+            { "dart", "Dart" },
+            { "r", "R" },
+            { "m", "MATLAB" },
+            { "f90", "Fortran" },
+            { "cob", "COBOL" },
+            { "ada", "Ada" },
+            { "lisp", "Lisp" },
+            { "scm", "Scheme" },
+            { "pro", "Prolog" },
+            { "hs", "Haskell" },
+            { "erl", "Erlang" },
+            { "groovy", "Groovy" },
+            { "scala", "Scala" },
+            { "html", "HTML/CSS" },
+            { "css", "HTML/CSS" },
+            { "sql", "SQL" },
+            { "asm", "Assembly language" },
+            { "vhd", "VHDL" },
+            { "v", "Verilog" },
+            { "sb3", "Scratch" },
+            { "blk", "Blockly" }
+        };
+
+            // Check if the extension exists in the mapping, and return the corresponding language
+            if (extensionToLanguage.TryGetValue(extension, out string language))
+            {
+                return language;
+            }
+            else
+            {
+                return "Unknown"; // Return "Unknown" for unrecognized extensions
+            }
+        }
+
+        static public bool checkAllGivenLangsValidation(List<string> givenLangs, List<string> validProgrammingLanguages)
+        {
+
+            foreach (var lang in givenLangs)
+            {
+                bool isValidLang = false;
+                foreach (var vLang in validProgrammingLanguages)
+                {
+                    if (lang == vLang)
+                    {
+                        isValidLang = true;
+                    }
+                }
+                if (isValidLang == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
     }
 }
